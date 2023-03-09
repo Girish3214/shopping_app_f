0452a6e962ec2bfe93f6740361f64379286b8e87
Girish <m.girishroy@gmail.com>
2023-03-09 21:42:23 +0530

    product details screen creation and named routes implementation

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
@@ -33,9 +34,17 @@ class ProductItem extends StatelessWidget {
             textAlign: TextAlign.center,
           ),
         ),
-        child: Image.network(
-          fit: BoxFit.cover,
-          imageUrl,
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: id,
+            );
+          },
+          child: Image.network(
+            fit: BoxFit.cover,
+            imageUrl,
+          ),
         ),
       ),
     );
